de4f4c24742a8eaac36e57a2164a25cfc56c5d05
benzouye <benzouye@hotmail.com>
2018-05-25 11:01:16 +0200

    Enhanced multiple maps display

--- fields/class-acf-field-openstreetmap-v4.php
+++ fields/class-acf-field-openstreetmap-v4.php
@@ -12,18 +12,23 @@ class acf_field_openstreetmap extends acf_field {
 		$this->label = 'OpenStreetMap';
 		$this->category = __('Content','acf'); // Basic, Content, Choice, etc
 		$this->defaults = array(
+			'address'		=> '',
 			'tiles'			=> 'osm_basic',
 			'height'		=> 500,
 			'center_lat'	=> 45.38350155204992,
 			'center_lng'	=> 4.501991271972657,
 			'zoom'			=> 11,
 			'min_zoom'		=> 2,
-			'max_zoom'		=> 18
+			'max_zoom'		=> 18,
+			'front_display'	=> 'map',
 		);
 		
 		parent::__construct();
 		
 		$this->settings = $settings;
+		
+		wp_enqueue_style( 'acf-leaflet', $this->settings['url'].'assets/css/leaflet.css' );
+		wp_enqueue_script( 'acf-leaflet', $this->settings['url'].'assets/js/leaflet.js' );
 	}
 	
 	function create_options( $field ) {
@@ -143,28 +148,53 @@ class acf_field_openstreetmap extends acf_field {
 ?>
 			</td>
 		</tr>
+		<tr class="field_option field_option_<?php echo $this->name; ?>">
+			<td class="label">
+				<label><?php _e("Front end display",'acf-openstreetmap'); ?></label>
+				<p><?php _e("Wether to display Leaflet map or just values front end",'acf-openstreetmap'); ?></p>
+			</td>
+			<td>
+<?php 
+				do_action('acf/create_field', array(
+					'type'		=>	'select',
+					'name'		=>	'fields[' .$key.'][front_display]',
+					'value'		=>	$field['front_display'],
+					'choices'	=>	array( 'map' => __('Map','acf-openstreetmap'), 'values' => __('Values','acf-openstreetmap') ),
+				));
+?>
+			</td>
+		</tr>
 <?php
 	}
 	
 	function create_field( $field ) {
-		$lat = $field['value'] ? $field['value']['center_lat'] : $field['center_lat'];
-		$lng = $field['value'] ? $field['value']['center_lng'] : $field['center_lng'];
+		$id = 'leaflet-'.$field['key'];
+		$lat = !empty($field['value']['center_lat']) ? $field['value']['center_lat'] : $field['center_lat'];
+		$lng = !empty($field['value']['center_lng']) ? $field['value']['center_lng'] : $field['center_lng'];
+		$address = !empty($field['value']['address']) ? $field['value']['address'] : $field['address'];
 		$tiles = $this->settings['layers'][$field['tiles']];
 ?>
 		<div class="acf-openstreetmap">
 			<input id="acf-openstreetmap-lat" type="hidden" name="<?php echo esc_attr($field['name']); ?>[center_lat]" value="<?php echo $lat; ?>" />
 			<input id="acf-openstreetmap-lng" type="hidden" name="<?php echo esc_attr($field['name']); ?>[center_lng]" value="<?php echo $lng; ?>" />
 			
-			<div id="leaflet-<?php echo $field['key']; ?>" style="height: <?php echo $field['height']; ?>px"></div>
+			<div class="acf-input-wrap"><input id="nominatim" type="text" name="<?php echo esc_attr($field['name']); ?>[address]" value="<?php echo $address; ?>" /></div>
+			
+			<div id="<?php echo $id; ?>" style="height: <?php echo $field['height']; ?>px"></div>
+			
 			<script>window.onload = function() {
+				
 				L.Icon.Default.imagePath = '<?php echo $this->settings['url'].'assets/images/'; ?>';
-				var acfLeafletMap = L.map( 'leaflet-<?php echo $field['key']; ?>' ).setView([<?php echo $lat; ?>,<?php echo $lng; ?>],<?php echo $field['zoom']; ?>);
+				
+				var acfLeafletMap = L.map( <?php echo $id; ?> ).setView([<?php echo $lat; ?>,<?php echo $lng; ?>],<?php echo $field['zoom']; ?>);
 				L.tileLayer( '<?php echo $tiles['url']; ?>', {
 					maxZoom: <?php echo $field['max_zoom']; ?>,
 					minZoom: <?php echo $field['min_zoom']; ?>,
 					attribution: '<?php echo $tiles['attribution']; ?>'
 				}).addTo(acfLeafletMap);
+				
 				var marker = L.marker([<?php echo $lat; ?>,<?php echo $lng; ?>]).addTo(acfLeafletMap);
+				
 				acfLeafletMap.on( 'click', function(e) {
 					acfLeafletMap.eachLayer( function(e) {
 						if( e instanceof L.Marker ) {
@@ -175,6 +205,14 @@ class acf_field_openstreetmap extends acf_field {
 					$("#acf-openstreetmap-lat").val( e.latlng.lat );
 					$("#acf-openstreetmap-lng").val( e.latlng.lng );
 				});
+				
+				/*$("#nominatim").autocomplete({
+					source: 'https://benoit.maiorino.fr/geocoding.php',
+					minLength: 3,
+					select: function( event, ui ) {
+						console.log( ui );
+					}
+				});*/
 			};</script>
 		</div>
 <?php
@@ -184,10 +222,6 @@ class acf_field_openstreetmap extends acf_field {
 		// vars
 		$url = $this->settings['url'];
 		$version = $this->settings['version'];
-		
-		// require the leaflet JS and CSS
-		wp_enqueue_style( 'acf-leaflet-css', plugin_dir_url( __FILE__ ).'../assets/css/leaflet.css' );
-		wp_enqueue_script( 'acf-leaflet-js', plugin_dir_url( __FILE__ ).'../assets/js/leaflet.js' );
 		
 		// register & include JS
 		wp_register_script('acf-openstreetmap-js', "{$url}assets/js/input.js", array('acf-input'), $version);
@@ -199,28 +233,27 @@ class acf_field_openstreetmap extends acf_field {
 	}
 	
 	function format_value_for_api( $value, $post_id, $field ) {
-		
-		// require the leaflet JS and CSS
-		wp_enqueue_style( 'acf-leaflet-css', plugin_dir_url( __FILE__ ).'../assets/css/leaflet.css' );
-		wp_enqueue_script( 'acf-leaflet-js', plugin_dir_url( __FILE__ ).'../assets/js/leaflet.js' );
-		
-		$lat = $field['value'] ? $field['value']['center_lat'] : $field['center_lat'];
-		$lng = $field['value'] ? $field['value']['center_lng'] : $field['center_lng'];
-		$tiles = $this->settings['layers'][$field['tiles']];
-		$value = '
-		<div id="leaflet-'.$field['key'].'" style="height: '.$field['height'].'px"></div>
-		<script>
-			window.onload = function() {
-				L.Icon.Default.imagePath = "'.$this->settings['url'].'assets/images/";
-				var acfLeafletMap = L.map( "leaflet-'.$field['key'].'" ).setView(['.$lat.','.$lng.'],'.$field['zoom'].');
-				L.tileLayer( "'.$tiles['url'].'", {
-					maxZoom: '.$field['max_zoom'].',
-					minZoom: '.$field['min_zoom'].',
-					attribution: \''.$tiles['attribution'].'\'
-				}).addTo(acfLeafletMap);
-				var marker = L.marker(['.$lat.','.$lng.']).addTo(acfLeafletMap);
-			}
-		</script>';
+		if( $field['front_display'] == 'map' ) {
+			
+			$id = 'leaflet-'.$field['key'].'-'.$post_id;
+			$lat = $field['value'] ? $field['value']['center_lat'] : $field['center_lat'];
+			$lng = $field['value'] ? $field['value']['center_lng'] : $field['center_lng'];
+			$tiles = $this->settings['layers'][$field['tiles']];
+			$value = '
+			<div id="'.$id.'" style="height: '.$field['height'].'px"></div>
+			<script>
+				window.addEventListener( "load", function() {
+					L.Icon.Default.imagePath = "'.$this->settings['url'].'assets/images/";
+					var acfLeafletMap'.$post_id.' = L.map( "'.$id.'" ).setView(['.$lat.','.$lng.'],'.$field['zoom'].');
+					L.tileLayer( "'.$tiles['url'].'", {
+						maxZoom: '.$field['max_zoom'].',
+						minZoom: '.$field['min_zoom'].',
+						attribution: \''.$tiles['attribution'].'\'
+					}).addTo(acfLeafletMap'.$post_id.');
+					var marker = L.marker(['.$lat.','.$lng.']).addTo(acfLeafletMap'.$post_id.');
+				});
+			</script>';
+		}
 		
 		return $value;
 	}
